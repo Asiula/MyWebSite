5e6614d366e415a21cc218a8012833c6d9428eff
Asiula <mad15@o2.pl>
2017-08-19 21:25:31 +0200

    del items from todo list

--- todo.js
+++ todo.js
@@ -1,8 +1,27 @@
+var array = []
 function todoList()
 {
     var item = document.getElementById('todoInput').value
     var text = document.createTextNode(item)
     var newItem = document.createElement('li')
+    array.push(item)
     newItem.appendChild(text)
     document.getElementById('todoList').appendChild(newItem)
+    document.getElementById('delInput').setAttribute("max",array.length)
+    document.getElementById('todoInput').value = ''
+
+}
+function delList()
+{
+    var item = document.getElementById('delInput').value
+    var  nan = isNaN(item)
+    if(nan==false){
+        var proto = item - 1
+        var lista =  document.getElementById('todoList')
+        lista.removeChild(lista.childNodes[proto])
+        array.splice(proto,1)
+        document.getElementById('delInput').setAttribute("max",array.length)
+        document.getElementById('delInput').value = ''
+    }
+    else {alert("Input is not a number")}
 }
